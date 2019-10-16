c671be20133ae999173e8810a010a66d3aac134b
Iury Lemos <iurylemos10@gmail.com>
2019-10-16 09:14:44 -0300

    Animação das ondas, e o viewChild corrigido para navegação

--- src/app/pages/login/login.page.ts
+++ src/app/pages/login/login.page.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
+import { IonSlides } from '@ionic/angular';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +8,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginPage implements OnInit {
 
+  @ViewChild(IonSlides, { read: IonSlides , static: true }) slides: IonSlides;
+  public wavesPosition: number = 0;
+  public wavesDifference: number = 80;
+
   constructor() { }
 
   ngOnInit() {
+    //Bloqueando a navegação de arrastar para o lado os slides.
+    // this.slides.lockSwipes(true);
+  }
+
+  segmentChanged(event: any) {
+    // console.log(event)
+    if(event.detail.value === "login") {
+      this.slides.slidePrev();
+      //Animação para as ondinhas
+      this.wavesPosition += this.wavesDifference;
+    }else {
+      this.slides.slideNext();
+      this.wavesPosition -= this.wavesDifference;
+    }
   }
 
 }
