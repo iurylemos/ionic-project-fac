c4f382372669c2c4de7bb90e876f211c170489e2
Iury Lemos <iurylemos10@gmail.com>
2019-10-20 02:10:06 -0300

    Deslogando com êxito no app

--- src/app/pages/home/home.page.ts
+++ src/app/pages/home/home.page.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Product } from 'src/app/interfaces/product';
 import { Subscription } from 'rxjs';
 import { ProductService } from 'src/app/services/product.service';
+import { AuthService } from 'src/app/services/auth.service';
 
 @Component({
   selector: 'app-home',
@@ -13,7 +14,10 @@ export class HomePage implements OnInit {
   private products = new Array<Product>();
   private productsSubscription: Subscription;
 
-  constructor(private productsService: ProductService) {
+  constructor(
+    private productsService: ProductService,
+    private authService: AuthService
+    ) {
     this.productsSubscription = this.productsService.getProducts().subscribe(data => {
       this.products = data;
     });
@@ -26,5 +30,13 @@ export class HomePage implements OnInit {
     //Called once, before the instance is destroyed.
     //Add 'implements OnDestroy' to the class.
     this.productsSubscription.unsubscribe();
+  }
+
+  public async logout() {
+    try {
+      await this.authService.logout()
+    }catch(error) {
+      console.error(error)
+    }
   }
 }
